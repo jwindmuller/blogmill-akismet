5e66d3a0ff9dcf9f13ac00fee4933e86f82cb59f
Joaquin Windmuller <joaquin@windmuller.ca>
2011-12-03 22:32:23 -0700

    Hooking into "comment_is_ham" to report incorrect detection of spam
    - Refactoring AkismetHooksComponent::newAkismet into a AkismetComponent

---
+++ controllers/components/akismet.php
@@ -0,0 +1,20 @@
+<?php
+App::import('Vendor', 'akismet.Akismet');
+class AkismetComponent extends Object {
+    public static function newAkismet($comment_data = null) {
+        $Setting = ClassRegistry::init('Setting');
+        $key = $Setting->get('Akismet.key');
+        $blogUrl = Router::url('/', true);
+        $akismet = new Akismet($blogUrl, $key);
+
+        if ( $comment_data ) {
+            extract( $comment_data['Comment'] );
+            $akismet->setCommentAuthor($name);
+            $akismet->setCommentAuthorEmail($email);
+            $akismet->setCommentAuthorURL($url);
+            $akismet->setCommentContent($content);
+        }
+        return $akismet;
+    }
+}
+
--- controllers/components/akismet_hooks.php
+++ controllers/components/akismet_hooks.php
@@ -1,29 +1,19 @@
 <?php
-App::import('Vendor', 'akismet.Akismet');
-class AkismetHooksComponent extends Object {
-    
-    private function newAkismet($comment_data = null) {
-        $Setting = ClassRegistry::init('Setting');
-        $key = $Setting->get('Akismet.key');
-        $blogUrl = Router::url('/', true);
-        $akismet = new Akismet($blogUrl, $key);
-        
-        if ( $comment_data ) {
-            extract( $comment_data['Comment'] );
-            $akismet->setCommentAuthor($name);
-            $akismet->setCommentAuthorEmail($email);
-            $akismet->setCommentAuthorURL($url);
-            $akismet->setCommentContent($content);
-        }
-        return $akismet;
-    }
+App::import('Component', 'Akismet.Akismet');
+class AkismetHooksComponent extends BlogmillHookComponent {
+
     public function before_comment(&$comment_data) {
-        $akismet = $this->newAkismet($comment_data);
+        $akismet = AkismetComponent::newAkismet($comment_data);
         $comment_data['Comment']['spam'] = $akismet->isCommentSpam();
     }
-    
+
     public function comment_is_spam($comment_data) {
-        $akismet = $this->newAkismet($comment_data);
+        $akismet = AkismetComponent::newAkismet($comment_data);
         $akismet->submitSpam();
+    }
+
+    public function comment_is_ham($comment_data) {
+        $akismet = AkismetComponent::newAkismet($comment_data);
+        $akismet->submitHam();
     }
 }
